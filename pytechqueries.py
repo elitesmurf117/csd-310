08aa28371a85859457af2e1bad6aea868b229e2a
elitesmurf117 <97317079+elitesmurf117@users.noreply.github.com>
2022-02-06 14:03:41 -0700

    Add files via upload

---
+++ pytechqueries.py
@@ -0,0 +1,25 @@
+MongoDB: insert pymongo 
+{ 
+    Student : [{
+    id : 1007,
+    first_name : Brandon,
+    last_name : Herrada,}],
+
+ }
+{ 
+    Student : [{
+    id : 1008,
+    first_name : John,
+    last_name : Smith,}],
+
+ }
+{ 
+    Student : [{
+    id : 1009,
+    first_name : Gamora,
+    last_name : Thanos,}],
+
+ }
+print (Student:id:1009)
+
+
